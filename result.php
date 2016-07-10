112f0800cb49180963c484cc12f12bacf9d969b9
yellowdinar <kalimullindinrust@gmail.com>
2016-07-11 02:29:40 +0300

    print test.html

--- result.php
+++ result.php
@@ -13,9 +13,25 @@ require_once 'amocrm/MysqliDb.php';
 $db = new MysqliDb ('localhost', 'root', 'root', 'brokers');
 
 $phone_numbers = $db->get('phone');
-$result = array();
+$result = '<!DOCTYPE html><html><head lang="ru"><meta charset="UTF-8"><title>Сравнение контактов</title></head><body><table>';
 foreach($phone_numbers as $phone) {
     $db->where ("phone_id", $phone['id']);
     $contacts = $db->get("contact");
-    echo print_r($contacts).'|||||||||||||';
+    $result .= '<tr><td>'.$phone["value"].'</td><td><table>';
+    foreach($contacts as $contact) {
+        $result .= '<tr><td><a href="https://brokerskazan.amocrm.ru/contacts/detail/'.$contact["contact_id"].'">'.$contact["name"].'</a></td></tr>';
+    }
+    $result .= '</table></td></tr>';
 }
+$result = '</table></body></html>';
+
+$file = fopen ("test.html","r+");
+if ( !$file )
+{
+    echo("Ошибка открытия файла");
+}
+else
+{
+    fputs ( $file, $result);
+}
+fclose ($file);
