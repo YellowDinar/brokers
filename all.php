18f065401d03b9801d8e05e949cee5be696e00ca
yellowdinar <kalimullindinrust@gmail.com>
2016-07-10 18:21:51 +0300

    phone numbers count

--- all.php
+++ all.php
@@ -8,6 +8,7 @@
 set_time_limit(0);
 ini_set('memory_limit', '1G');
 require_once 'amocrm/Amo2.php';
+require_once 'amocrm/MysqliDb.php';
 
 $amo = new Amo(null, null, null, null, null, null);
 $limit = 500;
@@ -24,16 +25,27 @@ while($limit === 500) {
     $sum += count($arr);
     $offset += 500;
 }
-
+$rrr = 0;
 foreach($data as $part) {
     foreach($part as $contact) {
         foreach($contact['custom_fields'] as $field) {
             if (strcmp($field['id'], '803138') === 0) {
                 foreach($field['values'] as $value) {
                     echo $value['value'].', ';
+                    $rrr += 1;
                 }
                 echo '||||||||||';
             }
         }
     }
 }
+
+
+echo '====== '.$rrr.' =======';
+
+//$db = new MysqliDb ('localhost', 'kostiasplin_amo', '88005003360', 'kostiasplin_amo');
+//$insert_data = Array (
+//    "contact_id" => $contacts[0]['id'],
+//    "timestamp" => date(time())
+//);
+//$db->insert ('contact', $insert_data);
