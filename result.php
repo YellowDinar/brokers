75bf190d42e0226fa222bf308f607fc1fe13cc98
yellowdinar <kalimullindinrust@gmail.com>
2016-07-11 13:23:25 +0300

    alpha

--- result.php
+++ result.php
@@ -19,9 +19,9 @@ $db = new MysqliDb ('localhost', 'root', 'root', 'brokers');
 
 $phone_numbers = $db->get('phone');
 $result = '<!DOCTYPE html><html><head lang="ru"><meta charset="UTF-8"><link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"><title>Сравнение контактов</title></head><body><div class="container"><table class="table">';
-$k = 0;
+//$k = 0;
 foreach($phone_numbers as $phone) {
-    if ($k < 500) {
+//    if ($k < 500) {
         $db->where("phone_id", $phone['id']);
         $contacts = $db->get("contact");
     //    foreach($contacts as $contact) {
@@ -34,9 +34,9 @@ foreach($phone_numbers as $phone) {
                 $result .= '<tr><td><a target="_blank" href="https://brokerskazan.amocrm.ru/contacts/detail/' . $contact["contact_id"] . '">' . $contact["name"] . '</a></td></tr>';
             }
             $result .= '</table></td></tr>';
-            $k++;
+//            $k++;
         }
-    }
+//    }
 }
 $result .= '</table></div><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script></body></html>';
 $file = fopen ($_SERVER['DOCUMENT_ROOT']."result.html", "r+");
