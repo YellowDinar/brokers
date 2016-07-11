ba03a33496a167dbc5cd3970c4c7f9a5c79169f4
yellowdinar <kalimullindinrust@gmail.com>
2016-07-11 10:35:46 +0300

    permision fix

--- result.php
+++ result.php
@@ -37,7 +37,7 @@ foreach($phone_numbers as $phone) {
     }
 }
 $result = '</table></body></html>';
-$file = fopen ("test.html", "w");
+$file = fopen ($_SERVER['DOCUMENT_ROOT']."test.html", "w");
 if ( !$file )
 {
     echo("Ошибка открытия файла");
