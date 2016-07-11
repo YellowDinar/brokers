20c6f3bc1dec69d3550a7619478101d3cded50e4
yellowdinar <kalimullindinrust@gmail.com>
2016-07-11 12:18:09 +0300

    clear result.html and fixed html in result.php

--- result.php
+++ result.php
@@ -18,25 +18,27 @@ require_once 'amocrm/MysqliDb.php';
 $db = new MysqliDb ('localhost', 'root', 'root', 'brokers');
 
 $phone_numbers = $db->get('phone');
-$result = '<!DOCTYPE html><html><head lang="ru"><meta charset="UTF-8"><title>Сравнение контактов</title></head><body><table>';
+$result = '<!DOCTYPE html><html><head lang="ru"><meta charset="UTF-8"><link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"><title>Сравнение контактов</title></head><body><div class="container"><table class="table">';
 $k = 0;
 foreach($phone_numbers as $phone) {
-    if ($k < 1000) {
+    if ($k < 500) {
         $db->where("phone_id", $phone['id']);
         $contacts = $db->get("contact");
     //    foreach($contacts as $contact) {
     //        echo $contact["contact_id"].', '.$contact["name"];
     //    }
     //    echo '|||';
-        $result .= '<tr><td>' . $phone['value'] . '</td><td><table>';
-        foreach ($contacts as $contact) {
-            $result .= '<tr><td><a href="https://brokerskazan.amocrm.ru/contacts/detail/' . $contact["contact_id"] . '">' . $contact["name"] . '</a></td></tr>';
+        if(count($contacts) > 1) {
+            $result .= '<tr><td>' . $phone['value'] . '</td><td><table>';
+            foreach ($contacts as $contact) {
+                $result .= '<tr><td><a href="https://brokerskazan.amocrm.ru/contacts/detail/' . $contact["contact_id"] . '">' . $contact["name"] . '</a></td></tr>';
+            }
+            $result .= '</table></td></tr>';
+            $k++;
         }
-        $result .= '</table></td></tr>';
-        $k++;
     }
 }
-$result .= '</table></body></html>';
+$result .= '</table></div><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script></body></html>';
 $file = fopen ($_SERVER['DOCUMENT_ROOT']."result.html", "r+");
 if ( !$file )
 {
